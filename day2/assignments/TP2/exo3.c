c61a85b90d1f9089367aac07914f69eb8f534c39
ngbinetou <binetoucecile20022@gmail.com>
2022-03-18 11:08:19 +0100

    add TP2

---
+++ day2/assignments/TP2/exo3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int   i =  8;
+	int   j =  5;
+	float x =  0.005f;
+	float y = -0.01f;
+	char  c = 'c';
+	char  d = 'd';
+	int result1=(int)(3*i-2*j)%(2*d-c); 
+	printf("%d",result1);
+
+
+
+	return 0;
+}
